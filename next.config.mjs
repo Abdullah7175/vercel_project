8161c9200f41badc57128f470ac96037fc2e223b
Abdullah7175 <abdullah7175@gmail.com>
2025-03-21 22:08:51 +0500

    Fix Cognito login and Vercel default page

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-// /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: "/",
-          destination: "/login",
-          permanent: true,
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
