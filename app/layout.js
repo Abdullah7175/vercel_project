76191c8b180216927d0d7641bd93d3c3bdb48bc1
Abdullah7175 <abdullah7175@gmail.com>
2025-03-22 23:40:44 +0500

    pushed

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ const cognitoAuthConfig = {
   client_id: "7n47e85nrdqahvqhoq9d4ttd9j",
   redirect_uri: "https://vercel-project-gilt.vercel.app/dashboard",
   response_type: "code",
-  scope: "email openid profile",
+  scope: "email openid",
 };
 
 export default function RootLayout({ children }) {
