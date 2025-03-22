004882a34c6f6d872a0efd614cd42ffd6a0c7e8d
Abdullah7175 <abdullah7175@gmail.com>
2025-03-23 00:39:22 +0500

    comit logout

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import "./globals.css";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_BktrR8B1L",
   client_id: "7n47e85nrdqahvqhoq9d4ttd9j",
-  redirect_uri: "https://vercel-project-em6n.vercel.app/dashboard",
+  redirect_uri: "https://vercel-project-em6n.vercel.app",
   response_type: "code",
   scope: "openid email profile",
 };
