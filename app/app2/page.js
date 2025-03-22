ea7bd25242152a29e75a34aba670f0c3c200114a
Abdullah7175 <abdullah7175@gmail.com>
2025-03-23 00:46:12 +0500

    logout

--- app/app2/page.js
+++ app/app2/page.js
@@ -32,19 +32,6 @@ function App() {
     return <div>Encountering error... {auth.error.message}</div>;
   }
 
-  // if (auth.isAuthenticated) {
-  //   return (
-  //     <div>
-  //       <pre> Hello: {auth.user?.profile.email} </pre>
-  //       <pre> ID Token: {auth.user?.id_token} </pre>
-  //       <pre> Access Token: {auth.user?.access_token} </pre>
-  //       <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
-  //       <button onClick={() => auth.removeUser()}>Sign out</button>
-  //     </div>
-  //   );
-  // }
-
   if (auth.isAuthenticated) {
     // After successful login, redirect user to dashboard
     router.push("/dashboard");
@@ -137,11 +124,6 @@ function App() {
       </div>
     </div>
   );
-    // <div>
-    //   <button onClick={() => auth.signinRedirect()}>Sign in</button>
-    //   <button onClick={() => signOutRedirect()}>Sign out</button>
-    // </div>
-
 }
 
 export default App;
